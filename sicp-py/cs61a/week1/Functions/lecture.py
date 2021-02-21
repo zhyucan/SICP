7f798489120e1e46172c3dead63d39934593138f
zhyucan <zhyucan@gmail.com>
2021-02-21 22:08:02 +0800

    hw01

---
+++ sicp-py/cs61a/week1/Functions/lecture.py
@@ -0,0 +1,69 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+log = print
+
+
+# λa,x,b.ax+b
+# the Greek letter lambda
+
+# def λ(a, x, b):
+#     return a * x + b
+#
+#
+# log(λ(1, 2, 3))
+
+# lambda a, x, b: a * x + b
+
+
+# pure functions
+# output depends only on input
+
+# inpure functions
+# side effects besides return
+# hidden-variable
+
+# f = min
+# f = max
+# g, h = min, max
+# max = g
+# log(max(f(2, g(h(1, 5), 3)), 4))
+"""
+假设 
+min 指向的内存地址是 100
+max 指向的内存地址是 001
+
+f = min
+f 指向 100
+
+f = max
+f 重新指向 001
+
+g, h = min, max
+g 指向 100, h 指向 001
+
+max = g
+max 重新指向 100
+f h 指向 001 不变
+
+btw 内存地址 --> memory address
+"""
+
+# log(min(max(2, min(max(1, 5), 3)), 4))
+
+
+# x = 1
+# y = x
+# x = 2
+# x, y = x + y, x
+#
+# log(x, y)
+
+# x = 1
+# y = x
+# x = 2
+# x = x + y
+# y = x
+#
+# log(x, y)
+
---
+++ sicp-py/cs61a/week1/Introduction/lecture.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+log = print
+
+
+# Nested call Expression
+# 1 Evaluate operator
+# 2 Evaluate operands
+# 3 Apply
+
